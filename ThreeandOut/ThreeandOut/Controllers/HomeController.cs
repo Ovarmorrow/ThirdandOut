a21862d87508d7e4eb0d79200c2b7300cc56c83f
Ovarmorrow <medeconzo@gmail.com>
2017-06-07 22:25:23 -0400

    Initial Commit

---
+++ ThreeandOut/ThreeandOut/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System.Web.Mvc;
+
+namespace ThreeandOut.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "3AO.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact Us.";
+
+            return View();
+        }
+        public ActionResult Schedule()
+        {
+            ViewBag.Message = "Most Current Schedule.";
+
+            return View();
+        }
+        public ActionResult Highlights()
+        {
+            ViewBag.Message = "Highlight videos from the most recent year.";
+
+            return View();
+        }
+        public ActionResult Roster()
+        {
+            ViewBag.Message = "A current full team roster.";
+
+            return View();
+        }
+    }
+
+}
